7910f24c8a463d8821c9164b39046eef55cb0c26
Bendahman Nihed <nihed.bendahman@etu.umontpellier.fr>
2020-04-20 20:36:06 +0200

    modif

---
+++ additionalRows.py
@@ -0,0 +1,43 @@
+import sys
+import pandas as pd
+#import urllib2
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import dateparser
+import Claim as claim_obj
+from selenium import webdriver
+from nltk import word_tokenize
+import nltk
+#nltk.download('punkt')
+from nltk.corpus import wordnet as wn
+from py_thesaurus import Thesaurus
+import brouillon
+
+
+
+
+def briefAdditionalRows(soup, body,url, idClaim, relp, rubri, l, t):
+
+	claim_ =  claim_obj.Claim()	
+	claim = soup.find('div', {"class": "col-xs-12 col-sm-6 col-left"})
+	if claim :
+		claim_.setSource("fullfact")
+		claim_.setUrl("http://fullfact.org"+url.replace("?utm_source=content_page&utm_medium=related_content",""))
+		claim_.setClaim(claim.get_text().replace("\nClaim\n",""))
+		claim_.setIdClaim(idClaim)
+		claim_.setRubrique(rubri)
+		claim_.setKeyWordsRP(l)
+		claim_.setTitle(t)
+		
+
+		conclusion = soup.find('div', {"class": "col-xs-12 col-sm-6 col-right"})
+		if conclusion :
+			claim_.setConclusion(conclusion.get_text().replace("\nConclusion\n",""))
+			c=conclusion.get_text().replace("\nConclusion\n","")
+			claim_.setVerdictTompo(brouillon.fonctionPrincipale(c))
+
+		claim_.setBody(body.get_text())
+		claim_.setRelated_posts(relp) 
+		return claim_
+	else:
+		return "empty"
