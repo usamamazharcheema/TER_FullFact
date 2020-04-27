ab3210831bc1494f014ff47f76202c4202ad073b
Djelouat Maroua Dorsaf <maroua-dorsaf.djelouat@etu.umontpellier.fr>
2020-04-27 15:34:48 +0200

    modification lienPosts

--- lienPosts.py
+++ lienPosts.py
@@ -1,4 +1,3 @@
-#Les imports necessaires au programme.
 import sys
 #import import_ipynb
 import pandas as pd
@@ -8,7 +7,7 @@ import nltk
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
-import TraitementConclusion
+import brouillon
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import cdist
@@ -24,36 +23,17 @@ from yellowbrick.cluster import KElbowVisualizer
 negation=["no", "not","neither", "nor"]
 pays=["eu", "uk", "us"]
 ponctuation=[".",",","!",";","?", "'", "â€˜", "â€™"]
-mots=["people", "new", "list", "years", "days"]
+mots=["people", "new", "list", "years", "days", "show"]
 categorie=["economy", "health", "europe", "online"]
 stop_words = stopwords.words('english')
-
-rubHealth=[]
-rubEconomy=[]
-rubEurope=[]
-rubOnline=[]
-def loadData(nomRubrique, nomFichier):
- 
-    with open(nomFichier, 'rb') as fichier:
-        mon_depickler = pickle.Unpickler(fichier)
-        nomRubrique=mon_depickler.load()
-    fichier.close()
-    return nomRubrique
-
-
-rubHealth=loadData(rubHealth,'health')
-rubEconomy=loadData(rubEconomy,'economy')
-rubEurope=loadData(rubEurope,'europe')
-rubOnline=loadData(rubOnline,'online')
-
 
 
 def traitementPosts(post):
     tempo=[]
     resultat=[]
-    pos= TraitementConclusion.nettoyage(post)
-    pos=TraitementConclusion.lemmatization(pos)
-    result= TraitementConclusion.eliminationStopWords(pos)
+    pos= brouillon.nettoyage(post)
+    pos=brouillon.lemmatization(pos)
+    result= brouillon.eliminationStopWords(pos)
     result= [word for word in result if word not in ponctuation and word not in mots]  
     for r in result:
         if ("-" in r):
@@ -75,7 +55,7 @@ def traitementPosts(post):
 
 
 
-def traitementRelatedPosts(relp,keyWords, posts):
+def traitementRelatedPosts(relp, keyWords, posts):
     
     for v in relp:
         if(v[0] not in categorie and v[0] not in keyWords):
@@ -87,18 +67,32 @@ def traitementRelatedPosts(relp,keyWords, posts):
         posts.append(ph)
      
     return posts
+ 
+def traitementPostsRubrique(keyWords, posts, claims):
+    
+    for c in claims:
+        if(c.getRubrique() not in categorie and v[0] not in keyWords):
+            keyWords.append(c.getRubrique())
+        post= c.getTitle() + " " + c.getClaim()
+        post= post.replace(".", "")
+        #print(post)
+        ph=traitementPosts(post)
+        posts.append(ph)
+     
+    return posts
+ 
 
 
 def getLiensPosts(posts, true_k, rubrique):
     
     vectorizer = TfidfVectorizer()
- 
+    res= vectorizer.fit_transform(posts)
 
     df = pd.DataFrame(
     data=vectorizer.fit_transform(posts).toarray(),
     columns=vectorizer.get_feature_names()
     )
-    res= vectorizer.fit_transform(posts)
+   
   
 
     model = KMeans(n_clusters=true_k)
@@ -110,8 +104,8 @@ def getLiensPosts(posts, true_k, rubrique):
     #print(model.labels_)
     #print(kmeans_indices)
     
-    '''
     #affichage des points avec PCA
+    '''
     pca = PCA(n_components=2).fit(res.todense())
     data2D = pca.transform(res.todense())
     centers = np.array(pca.transform(model.cluster_centers_))
@@ -123,53 +117,87 @@ def getLiensPosts(posts, true_k, rubrique):
     plt.figure(figsize=(16,8))
   
     plt.show()
-    '''
 
+'''
    #affichage des top mots clés par cluster
     order_centroids = model.cluster_centers_.argsort()[:, ::-1]
     terms = vectorizer.get_feature_names()
   
 
-    print("Top terms per cluster ( " +rubrique+" )")
+    #print("Top terms per cluster ( " +rubrique+" )")
     topMC=[]
     for i in range(true_k):
         topMC.append([])
         for ind in order_centroids[i, :5]:
-            print(terms[ind])
+            #print(terms[ind])
             topMC[i].append(terms[ind])
-        print("-----end cluster-----")
+        #print("-----end cluster-----")
     #print (topMC)
 
     return topMC, kmeans_indices
   
 
-def recuperationElementsPredits(relp, topMC, indices, true_k):
+def recuperationElementsPredits(topMC, indices, true_k, claims):
+
+    
+    cc=[]
+    c={}
+    for k in range(true_k):
+        c[k]=[]
+        r=0
+        while r < len(indices) :
+            if indices[r]==k:
+                c[indices[r]].append(claims[r])
+            r+=1
+        c[k].append(topMC[k])
+   
+    for valeur in c.values(): 
+        print(valeur)
+        i=0
+        while i < len(valeur)-1:
+            valeur[i].setRelated_posts( [w.getUrl() for w in valeur if w!=valeur[i] and w != valeur[len(valeur) -1]])
+            valeur[i].setKeyWordsRP(valeur[len(valeur)-1])
+            cc.append(valeur[i].getDict())
+            i+=1
+    
+    print("Je suis dans lienPosts")
+    print("\n")
+    #print (cc)
+    print("++++++++++++++++++++++++++++++++++++++++++++")
+    return cc
+
+
+def recuperationRelatedPosts(relp, topMC, indices, true_k):
     i=0
     groupe=[]
-    k=0
-    for r in relp:
-        print(str(r[1])+"  ============> "+str(indices[k]))
-        k+=1
         
     for i in range(true_k):
-        groupe.append([])
         r=0
         while r < len(indices) :
             if indices[r]==i:
-                groupe[i].append(relp[r][1])
+                groupe.append(relp[r][1])
             r+=1
-        groupe[i].append(topMC[i])
-            
+        groupe.append(topMC[i])
     return groupe
-    
-def fonctionPRelatedPosts(relp, true_k, rubrique):
+   
+   
+def fonctionLiensRubrique(true_k, rubrique, claims):
+    posts=[]
+    keyWords=[]
+    #true_k=3
+    retour= traitementPostsRubrique(keyWords, posts,claims)
+    topMC, indices=getLiensPosts(retour, true_k, rubrique)
+    cc=recuperationElementsPredits(topMC, indices, true_k, claims)
+    return cc
+
+def fonctionLiensRelatedPosts(relp, true_k, rubrique):
     posts=[]
     keyWords=[]
     #true_k=3
     retour= traitementRelatedPosts(relp,keyWords, posts)
     topMC, indices=getLiensPosts(retour, true_k, rubrique)
-    rlp=recuperationElementsPredits(relp, topMC, indices, true_k)
-  
-    
+    rlp=recuperationRelatedPosts(relp, topMC, indices, true_k)    
     return rlp
+
+
 
