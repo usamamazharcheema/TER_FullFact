b493164d9142983d264853091b56e572e7637a38
Djelouat Maroua Dorsaf <maroua-dorsaf.djelouat@etu.umontpellier.fr>
2020-04-29 13:17:51 +0200

    modif claim

--- Claim.py
+++ Claim.py
@@ -17,6 +17,7 @@ class Claim:
 		self.html=False
 		self.idClaim=1
 		self.keyWordsRP=[]
+		self.liens_revue=""
 
 
 
@@ -86,6 +87,10 @@ class Claim:
 	def getRubrique(self):
 		return self.rubrique
 
+	def setLiensRevue(self, str_):
+		self.liens_revue = unicode(str_)
+		return self
+
 	
 
 
@@ -104,6 +109,7 @@ class Claim:
 		dict_['idClaim']=self.idClaim
 		dict_['related_posts']=self.related_posts
 		dict_['keyWordsRP']=self.keyWordsRP
+		dict_['liens_revue']=self.liens_revue
 		#dict_['id']=self.composedClaim
 		if (self.html):
 			dict_['html']=self.html
